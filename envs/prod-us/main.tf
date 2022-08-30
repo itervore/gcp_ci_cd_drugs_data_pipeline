ef1fafa7ad568a85c1503020e34a9d038dc4950d
itervore <94967431+itervore@users.noreply.github.com>
2022-08-30 12:46:25 +0200

    Ingress data in gcs (#1)

--- envs/prod-us/main.tf
+++ envs/prod-us/main.tf
@@ -12,3 +12,11 @@ terraform {
 provider "google" {
   project = "${var.project}"
 }
+
+
+resource "google_storage_bucket" "ingress_bucket" {
+  name          = "gcp-ci-cd-drugs-data-pipeline-ingress-bucket"
+  force_destroy = false
+  location      = "US"
+  storage_class = "STANDARD"
+}

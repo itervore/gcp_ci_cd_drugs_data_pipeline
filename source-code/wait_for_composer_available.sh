61adf9ae5e2165fda545fe79c101a7ee544b5613
Clément Paillard <clement.paillard.pro@gmail.com>
2022-09-06 21:42:59 +0200

    fix bash script

--- source-code/wait_for_composer_available.sh
+++ source-code/wait_for_composer_available.sh
@@ -5,8 +5,9 @@ n=0
 until [[ $n -ge "${3}" ]]
 do
   status=0
-  res_state= gcloud composer environments describe "${1}" --location "${2}" | grep state | cut -d: -f2
-  if ((res_state=='RUNNING')); then
+  res_state=$( gcloud composer environments describe "${1}" --location "${2}" | grep state | awk '{print $2}')
+  echo "($n/${3}) state is : $res_state"
+  if [ "$res_state" == "RUNNING" ]; then
     break
   fi
   n=$(($n+1))
